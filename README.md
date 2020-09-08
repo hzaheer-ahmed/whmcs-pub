25a9c60bd557d8d7109dae90fba2bcc060687136
hzaheer-ahmed <65111236+hzaheer-ahmed@users.noreply.github.com>
2020-09-08 12:32:56 +0400

    initial-commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kworx-whmcs
+ 
---
+++ configuration.php
@@ -0,0 +1,14 @@
+<?php
+$license = 'WHMCS-7c3bfaddef3bfbaad68e';
+$db_host = 'whmcs-db.cjeodcfnwzc4.eu-west-1.rds.amazonaws.com';
+$db_port = '3306';
+$db_username = 'kw_admin';
+$db_password = 'HSJA2pYj1KqZ2';
+$db_name = 'kw_whmcs_db';
+$cc_encryption_hash = 'tPBjBb5GryyXsiX6J1BXsqHP2XNJ70y4xGDqhTjUBySbGLv1O9LgQ7TYYKZHpdgs';
+$templates_compiledir = 'templates_c';
+$mysql_charset = 'utf8';
+$session_handling = 'database';
+$crons_dir = '/var/www/html/whmcs_crons/';
+$customadminpath = 'staff';
+#$smtp_debug = true;
